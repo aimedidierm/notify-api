fd34b9b7af43f597a0658a8c2088b29d3949c0b1
Aime Didier <aimedidiermugisha@gmail.com>
2024-08-10 23:55:20 +0200

    Removing test route

--- routes/api.php
+++ routes/api.php
@@ -4,26 +4,8 @@ use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use Kreait\Firebase\Factory;
-use Kreait\Firebase\Messaging\CloudMessage;
-use Kreait\Firebase\Messaging\Notification;
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('todos', TodoController::class)->only('index', 'store', 'show', 'destroy');
-
-Route::get('/test', function () {
-
-    $factory = (new Factory)
-        ->withServiceAccount(config('filesystems.disks.local.root') . '/' . env('FIREBASE_CREDENTIALS'));
-
-    $messaging = $factory->createMessaging();
-
-    $notification = Notification::create('Test Title', 'Test Body');
-    $message = CloudMessage::withTarget('topic', 'todos')
-        ->withNotification($notification);
-
-    $messaging->send($message);
-});
